3745aaeb4aacaf36461d175fd7ef7a0ed712be35
Amandeep Ghai <amandeep@keepworks.com>
2018-06-17 02:34:32 +0530

    Ensure that type is set either via field or within the resolver and automatic type resolution is run only when type_or_field and function.type is nil

--- lib/graphql/sugar/define/resolver.rb
+++ lib/graphql/sugar/define/resolver.rb
@@ -3,9 +3,14 @@ module GraphQL
     module Define
       module Resolver
         def self.call(type_defn, field_name, type_or_field = nil, desc = nil, **kwargs, &block) # rubocop:disable Metrics/ParameterLists
-          type_or_field ||= kwargs[:type] if !kwargs[:type].nil?
+          # Automatically determine function
+          function_class = Sugar.get_resolver_function(field_name)
+          kwargs[:function] ||= function_class.new
+          kwargs[:resolve] ||= ->(obj, args, ctx) { function_class.new.call(obj, args, ctx) }
 
-          if type_or_field.nil?
+          raise "You can define `type` either within #{function_class} or as an argument for #{field_name} field" if kwargs[:function].type && type_or_field
+
+          if type_or_field.nil? && kwargs[:function].type.nil?
             # Automatically determine type
             type_or_field ||= Sugar.get_resolver_graphql_type(field_name)
 
@@ -14,11 +19,6 @@ module GraphQL
             plural = Sugar.get_resolver_plural(field_name) if plural.nil?
             type_or_field = type_or_field.to_list_type.to_non_null_type if plural
           end
-
-          # Automatically determine function
-          function_class = Sugar.get_resolver_function(field_name)
-          kwargs[:function] ||= function_class.new
-          kwargs[:resolve] ||= ->(obj, args, ctx) { function_class.new.call(obj, args, ctx) }
 
           GraphQL::Define::AssignObjectField.call(type_defn, field_name, type_or_field, desc, **kwargs, &block)
         end
